3516e4962a74e2252d4df1d637b1489b1920652f
philhawker <philhawker1211@gmail.com>
2020-04-09 17:58:31 -0600

    changed original main.sccs to sub-main

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,7 +8,7 @@ import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import "./style/main.scss"
+import "./assets/scss/main.scss"
 
 function main() {
   ReactDOM.render(
