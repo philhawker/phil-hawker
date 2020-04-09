32b853f9753c6c196a62cbab622b099aff1ae7e0
philhawker <philhawker1211@gmail.com>
2020-04-09 16:38:21 -0600

    added second main.scss import

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -9,6 +9,7 @@ import reducers from "./reducers";
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
+import "./assets/scss/main.scss";
 
 function main() {
   ReactDOM.render(
