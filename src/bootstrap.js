3e5e213cdbcc85f8b427aad3f8536837a5cb793c
philhawker <philhawker1211@gmail.com>
2020-04-09 17:23:36 -0600

    import change

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -9,6 +9,7 @@ import reducers from "./reducers";
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss"
+import "./assets/css/main.scss"
 
 function main() {
   ReactDOM.render(
