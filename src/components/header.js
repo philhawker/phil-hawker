8a76087fef38cec25569aa1708216b2192cb3617
philhawker <philhawker1211@gmail.com>
2020-05-04 11:37:51 -0600

    adjusted header text color. darkened card shadow

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = (_props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
         <h1>PHIL HAWKER</h1>
-        <p>I'm a husband, a father, and a Full Stack Engineer.<br /> I'm also a meme enthusiast. <br />
+        <p style={{ color: "rgb(111, 145, 170)" }}>I'm a husband, a father, and a Full Stack Engineer.<br /> I'm also a meme enthusiast. <br />
         See my <a href="https://www.facebook.com/phil.hawker.7">facebook page</a> for further insite on my memes.</p>
     </header>
 )
